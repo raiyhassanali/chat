2cea03d5f49234b3771a5a5a8ff214087442f97a
Muhammad Hassan <raiyhassan909@gmail.com>
2026-02-21 09:24:35 +0500

    Your commit message

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,9 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
